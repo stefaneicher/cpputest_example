87213d7cb2f6116a65ed8e95ae5829afed1f385c
Stefan Eicher <stefan.eicher@ypsomed.com>
2019-09-20 15:44:06 +0200

    update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,2 +1,1 @@
-C:\YPM\Cygwin\32\YSP000001\010000\ApplicationData\bin\bash.exe -l -c "cd %CD:\=/%/; cmake.exe -DCMAKE_BUILD_TYPE=Debug ."
-
+C:\YPM\Cygwin\32\YSP000001\010000\ApplicationData\bin\bash.exe -l -c "cd %CD:\=/%/; mkdir build; cd build; cmake ..; make; make test"
