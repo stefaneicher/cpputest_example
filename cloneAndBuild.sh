df8f11d3c3e795d3a22db567eb81666f76269f89
Stefan Eicher <stefan.eicher@gmail.com>
2019-09-23 09:29:50 +0200

    update scripts

--- cloneAndBuild.sh
+++ cloneAndBuild.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
-dir=8
+start=`date +%s`
+
+
+#cd ..
+dir=cpputest_example2
 rm -r $dir
 cd ..
 git clone https://github.com/stefaneicher/cpputest_example.git $dir
 cd $dir
 
 ./run.sh
+
+end=`date +%s`
+echo Execution time was `expr $end - $start` seconds.
