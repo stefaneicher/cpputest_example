0c138fdf0f62ee330dc9e87ce1c2115453a45ca5
Stefan Eicher <stefan.eicher@gmail.com>
2019-09-26 07:26:28 +0200

    update runInDocker.sh

--- runInDocker.sh
+++ runInDocker.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 containername=debug
-
-#docker exec -i debug echo "I'm inside the container!"
-docker exec -i $containername echo "I'm inside the container!"
 docker exec -i $containername ./run.sh
