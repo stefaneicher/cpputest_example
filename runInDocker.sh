b8ed59fb607a34286dd5c64ae24793e7b0d7ac0e
Stefan Eicher <stefan.eicher@gmail.com>
2019-09-27 08:14:05 +0200

    run in runInDocker.sh

--- runInDocker.sh
+++ runInDocker.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
+
+#build and start docker-compose
+docker-compose -f ./docker-compose.yml up -d --build
+
+#build project
 containername=debug
 docker exec -i $containername ./run.sh
