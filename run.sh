082f4f399651ab863c544d22b87ae78f091744a9
Stefan Eicher <stefan.eicher@gmail.com>
2019-10-11 09:36:37 +0200

    use Unix Makefiles

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ rm -r build
 mkdir build
 cd build
 
-cmake -DCMAKE_BUILD_TYPE=Debug -G "CodeBlocks - Unix Makefiles" ..
+cmake -DCMAKE_BUILD_TYPE=Debug -G "Unix Makefiles" ..
 
 cmake --build . --target all -- -j 4
 ./tests/pythagorean_ut/pythagorean_ut
